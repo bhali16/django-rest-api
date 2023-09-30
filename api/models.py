93bda1c34b68190066c7df564ccd7b3367995930
Waqar Ahmad Khan <waqarbinmuhammad@gmail.com>
2023-09-30 20:18:01 +0500

    Created Book Model

--- api/models.py
+++ api/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    number_of_pages = models.IntegerField()
+    publish_date = models.DateField()
+    quantity = models.IntegerField()
+    
+    def __str__(self):
+        return self.title  
