1190db5506951cd39c4405346f28a131fdadbcda
Waqar Ahmad Khan <waqarbinmuhammad@gmail.com>
2023-10-01 20:35:04 +0500

    Feat: Add Book, View Books and single Book

--- api/views.py
+++ api/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from api.models import Book
-from django.http import JsonResponse
+from api.serializer import BookSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-def book_list(request):
+@api_view(['GET'])
+def get_books(request):
     books = Book.objects.all()
-    bookPython = list(books.values())
-    return JsonResponse({
-        'books': bookPython
-    })
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def add_book(request):
+    serializer = BookSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(serializer.errors)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def book(request, pk):
+    if request.method == 'GET':
+        book = Book.objects.get(id=pk)
+        serializer = BookSerializer(book)
+        return Response(serializer.data)
