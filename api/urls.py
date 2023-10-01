f6476161a575d6bab3dab52faad894e4e42aa2a3
Waqar Ahmad Khan <waqarbinmuhammad@gmail.com>
2023-10-01 20:35:14 +0500

    Updated Urls

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from api.views import book_list
+from api.views import get_books, book, add_book
 
 urlpatterns = [
-    path('list', book_list, name="booklist")
+    path('', get_books, name="get-books"),
+    path('<int:pk>',book, name='book'),
+    path('add', add_book, name="add-book"),
 ]
